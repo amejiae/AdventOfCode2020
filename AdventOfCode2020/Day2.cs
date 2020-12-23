b31ae8666f18b627c3e313016dd3ac5631c103c8
Alejandro Mejia <alejaso15@hotmail.com>
2020-12-23 20:58:51 +0100

    Day 2 completed

--- AdventOfCode2020/Day2.cs
+++ AdventOfCode2020/Day2.cs
@@ -2,37 +2,75 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 
 namespace AdventOfCode2020
 {
     public class Day2 : ProgramBase
     {
+        private readonly IEnumerable<PasswordEntry> _input;
+
+        public Day2()
+        {
+            _input = GetInputAsList();
+        }
         public override void Solve()
         {
-            var input = GetInputAsList();
-            List<PasswordEntry> validPasswords = new List<PasswordEntry>();
-            List<PasswordEntry> invalidPasswords = new List<PasswordEntry>();
+            SolvePart1();
+            SolvePart2();
+        }
 
+        private void SolvePart1()
+        {
+            var input = GetInputAsList();
+            int validPasswords = 0;
             foreach (PasswordEntry passwordEntry in input)
             {
-                if (IsPasswordValid(passwordEntry))
+                if (IsPasswordValidPart1(passwordEntry))
                 {
-                    validPasswords.Add(passwordEntry);
-                }
-                else
-                {
-                    invalidPasswords.Add(passwordEntry);
+                    validPasswords++;
                 }
             }
 
             Console.WriteLine(validPasswords);
         }
 
-        private bool IsPasswordValid(PasswordEntry passwordEntry)
+        private void SolvePart2()
         {
-            int characterOccurences = passwordEntry.Password.Count(f => f == passwordEntry.Character);
-            if (characterOccurences >= passwordEntry.Min && characterOccurences <= passwordEntry.Max)
+            int validPasswords = 0;
+            foreach (PasswordEntry passwordEntry in _input)
+            {
+                if (IsPasswordValidPart2(passwordEntry))
+                {
+                    validPasswords++;
+                }
+            }
+
+            Console.WriteLine(validPasswords);
+        }
+
+        private static bool IsPasswordValidPart1(PasswordEntry passwordEntry)
+        {
+            int characterOccurrences = passwordEntry.Password.Count(f => f == passwordEntry.Character);
+
+            if (characterOccurrences >= passwordEntry.Min && characterOccurrences <= passwordEntry.Max)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool IsPasswordValidPart2(PasswordEntry passwordEntry)
+        {
+            var positionOneValue = passwordEntry.Password.Substring(passwordEntry.Min - 1, 1);
+            var positionTwoValue = passwordEntry.Password.Substring(passwordEntry.Max - 1, 1);
+
+            if (positionOneValue == passwordEntry.Character.ToString() && positionTwoValue == passwordEntry.Character.ToString())
+            {
+                return false;
+            }
+
+            if (positionOneValue == passwordEntry.Character.ToString() || positionTwoValue == passwordEntry.Character.ToString())
             {
                 return true;
             }
