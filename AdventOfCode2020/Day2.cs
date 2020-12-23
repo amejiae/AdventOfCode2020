48252cb792a8e9b47056a8b377be23eb5073f5bb
Alejandro Mejia <alejaso15@hotmail.com>
2020-12-23 20:37:12 +0100

    Day 2 part one solved

--- AdventOfCode2020/Day2.cs
+++ AdventOfCode2020/Day2.cs
@@ -11,13 +11,18 @@ namespace AdventOfCode2020
         public override void Solve()
         {
             var input = GetInputAsList();
-            int validPasswords = 0;
+            List<PasswordEntry> validPasswords = new List<PasswordEntry>();
+            List<PasswordEntry> invalidPasswords = new List<PasswordEntry>();
 
             foreach (PasswordEntry passwordEntry in input)
             {
                 if (IsPasswordValid(passwordEntry))
                 {
-                    validPasswords ++;
+                    validPasswords.Add(passwordEntry);
+                }
+                else
+                {
+                    invalidPasswords.Add(passwordEntry);
                 }
             }
 
@@ -26,8 +31,8 @@ namespace AdventOfCode2020
 
         private bool IsPasswordValid(PasswordEntry passwordEntry)
         {
-            int characterOccurences = passwordEntry.Password.Split(passwordEntry.Character).Length - 1;
-            if (Enumerable.Range(passwordEntry.Min, passwordEntry.Max).Contains(characterOccurences))
+            int characterOccurences = passwordEntry.Password.Count(f => f == passwordEntry.Character);
+            if (characterOccurences >= passwordEntry.Min && characterOccurences <= passwordEntry.Max)
             {
                 return true;
             }
@@ -55,7 +60,7 @@ namespace AdventOfCode2020
             var entry = new PasswordEntry();
             entry.Min = int.Parse(line.Substring(0, line.IndexOf('-')));
             entry.Max = int.Parse(line.Substring(line.IndexOf('-') + 1, 2));
-            entry.Character = line.Substring(line.IndexOf(':') - 1, 1);
+            entry.Character = line.Substring(line.IndexOf(':') - 1, 1).ToCharArray().First();
             entry.Password = line.Substring(line.IndexOf(':') + 1).Trim();
 
             return entry;
@@ -66,7 +71,7 @@ namespace AdventOfCode2020
     {
         public int Min { get; set; }
         public int Max { get; set; }
-        public string Character { get; set; }
+        public char Character { get; set; }
         public string Password { get; set; }
 
         public override string ToString()
