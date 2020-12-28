a2548f9f86c549ad32589d8f34cd17afb07c91bb
Alejandro Mejia <alejaso15@hotmail.com>
2020-12-28 22:25:15 +0100

    Day 3 added

---
+++ AdventOfCode2020/Day3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    public class Day3 : ProgramBase
+    {
+        public override void Solve()
+        {
+            
+        }
+    }
+}
